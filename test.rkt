5745571437e063e57212d173dab16d5425678a91
sen <linyuansen@qq.com>
2015-06-03 00:13:55 +0800

    p72 eopl

--- test.rkt
+++ test.rkt
@@ -34,9 +34,10 @@
 
 ;-----------------------------------------------------------
 
-(define-datatype prpgram program?
+(define-datatype program program?
   (a-program
    (expl expression?)))
+
 (define-datatype expression expression?
   (const-exp
    (num number?))
@@ -50,6 +51,8 @@
    (exp2 expression?)
    (exp3 expression?))
   (var-exp
+   (var symbol?))
+  (let-exp
    (var symbol?)
    (exp1 expression?)
    (body expression?)))
@@ -81,5 +84,45 @@
 (define report-expval-extractor-error
   (lambda (type val)
     (eopl:error 'expval-> "~s type to ~s" val type)))
+
+
+(define run
+  (lambda (string)
+    ;(value-of-program (scan&parse string))))
+    (value-of-program string)))
+
+(define value-of-program
+  (lambda (pgm)
+    (cases program pgm
+      (a-program (exp1)
+                 (value-of exp1 (init-env))))))
+
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+      (const-exp (num) (num-val num))
+      (var-exp (var) (apply-env env var))
+      (diff-exp (exp1 exp2)
+                (let ((val1 (value-of exp1 env))
+                      (val2 (value-of exp2 env)))
+                  (let ((num1 (expval->num val1))
+                        (num2 (expval->num val2)))
+                    (num-val
+                     (- num1 num2)))))
+      (zero?-exp (exp1)
+                 (let ((val1 (value-of exp1 env)))
+                   (let ((num1 (expval->num val1)))
+                     (if (zero? num1)
+                         (bool-val #t)
+                         (bool-val #f)))))
+      (if-exp (exp1 exp2 exp3)
+              (let ((val1 (value-of exp1 env)))
+                (if (expval->bool val1)
+                    (value-of exp2 env)
+                    (value-of exp3 env))))
+      (let-exp (var exp1 body)
+               (let ((val1 (value-of exp1 env)))
+                 (value-of body
+                           (extend-env var val1 env)))))))
 
 
